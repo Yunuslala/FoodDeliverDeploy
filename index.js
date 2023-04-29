76b504b404ef7033a422e78f33b9dfa16c07b579
Yunuslala <saifjava2@gmail.com>
2023-04-29 14:29:49 +0530

    deployemnt data

---
+++ index.js
@@ -0,0 +1,20 @@
+const express=require("express");
+const app=express();
+require('dotenv').config();
+const {connection}=require("./Config/db");
+const {UserRouter}=require("./Router/User")
+const {ResturantRouter}=require("./Router/resurant");
+
+app.use(express.json());
+app.use(UserRouter);
+app.use(ResturantRouter);
+
+app.listen(process.env.port,async()=>{
+    try {
+        await connection
+        console.log("db is connected");
+    } catch (error) {
+        console.log("db is not connected");
+    }
+    console.log(`http://localhost:${process.env.port}`);
+})
